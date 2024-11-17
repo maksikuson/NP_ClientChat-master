7303ff82b85d977e7bdaa40a8863a1567561e5b6
maksikbata@gmail.com <maksikbata@gmail.com>
2024-11-17 19:05:13 +0200

    Add project files.

---
+++ ClientChat/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ClientChat
+{
+    public partial class MainWindow : Window
+    {
+        private IPEndPoint serverEndPoint;
+        private UdpClient client;
+        private ObservableCollection<string> messages = new ObservableCollection<string>();
+        private string userName;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = messages;
+            client = new UdpClient();
+            string address = ConfigurationManager.AppSettings["ServerAddress"];
+            short port = short.Parse(ConfigurationManager.AppSettings["ServerPort"]);
+            serverEndPoint = new IPEndPoint(IPAddress.Parse(address), port);
+        }
+
+        private void Join_Button_Click(object sender, RoutedEventArgs e)
+        {
+            userName = Microsoft.VisualBasic.Interaction.InputBox("Enter your nickname:", "Join Chat");
+            if (string.IsNullOrEmpty(userName))
+            {
+                MessageBox.Show("Nickname cannot be empty!");
+                return;
+            }
+
+            string message = $"$<join>{userName}";
+            SendMessage(message);
+
+            joinButton.IsEnabled = false;
+            leaveButton.IsEnabled = true;
+            Listen(); // Почати слухати сервер після приєднання
+        }
+
+        private void Leave_Button_Click(object sender, RoutedEventArgs e)
+        {
+            string message = "$<leave>";
+            SendMessage(message);
+
+            joinButton.IsEnabled = true;
+            leaveButton.IsEnabled = false;
+        }
+
+        private void Send_Button_Click(object sender, RoutedEventArgs e)
+        {
+            string message = msgTextBox.Text.Trim();
+            if (string.IsNullOrEmpty(message))
+            {
+                MessageBox.Show("Message cannot be empty!");
+                return;
+            }
+
+            SendMessage($"{userName}|{DateTime.Now:HH:mm:ss}|{message}");
+            msgTextBox.Clear();
+        }
+
+        private async void SendMessage(string message)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            await client.SendAsync(data, data.Length, serverEndPoint);
+        }
+
+        private async void Listen()
+        {
+            while (true)
+            {
+                var data = await client.ReceiveAsync();
+                string message = Encoding.UTF8.GetString(data.Buffer);
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    messages.Add(message);  // Додати отримане повідомлення до списку
+                });
+            }
+        }
+    }
+}
---
+++ ServerApp/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerApp
+{
+    public class ChatServer
+    {
+        const short port = 4040;
+        const string JOIN_CMD = "$<join>";
+        const string LEAVE_CMD = "$<leave>";
+        UdpClient server;
+        IPEndPoint clientEndPoint = null;
+        List<IPEndPoint> members;
+
+        public ChatServer()
+        {
+            server = new UdpClient(port);
+            members = new List<IPEndPoint>();
+        }
+
+        public void Start()
+        {
+            while (true)
+            {
+                byte[] data = server.Receive(ref clientEndPoint);
+                string message = Encoding.UTF8.GetString(data);
+                Console.WriteLine($"Message: {message} from {clientEndPoint} at {DateTime.Now}");
+
+                if (message.StartsWith(JOIN_CMD))
+                {
+                    string userName = message.Substring(JOIN_CMD.Length);
+                    AddMember(clientEndPoint, userName);
+                }
+                else if (message == LEAVE_CMD)
+                {
+                    RemoveMember(clientEndPoint);
+                }
+                else
+                {
+                    SendToAll(data);
+                }
+            }
+        }
+
+        private void AddMember(IPEndPoint member, string userName)
+        {
+            members.Add(member);
+            string welcomeMessage = $"{userName} has joined the chat!";
+            SendToAll(Encoding.UTF8.GetBytes(welcomeMessage));
+            Console.WriteLine("Member was added!");
+        }
+
+        private void RemoveMember(IPEndPoint member)
+        {
+            members.Remove(member);
+            string leaveMessage = $"{member.Address}:{member.Port} has left the chat.";
+            SendToAll(Encoding.UTF8.GetBytes(leaveMessage));
+            Console.WriteLine("Member has left.");
+        }
+
+        private void SendToAll(byte[] data)
+        {
+            foreach (var member in members)
+            {
+                server.SendAsync(data, data.Length, member);
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ChatServer server = new ChatServer();
+            server.Start();
+        }
+    }
+}
